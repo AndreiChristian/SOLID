d671f5deed91fcbf40cbc7b5317708912d60f746
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-20 20:13:09 +0200

    hello

--- first_app/src/App.jsx
+++ first_app/src/App.jsx
@@ -1,10 +1,31 @@
-import logo from "./logo.svg";
+import { createSignal } from "solid-js";
 import styles from "./App.module.css";
+import Navigation from "./Navigation";
 
 function App() {
+  const [count, setCount] = createSignal(0);
+
+
+
+  const increment = () => {
+    console.log("increment");
+    setCount(count() + 1);
+  };
+
+  const decrement = () => {
+    setCount(count() - 1);
+  };
+
+
+
   return (
     <div class={styles.App}>
-      <h1>Hello</h1>
+      <Navigation />
+      <div className={styles.container}>
+        <button onClick={decrement}>-</button>
+        <h1>{count()}</h1>
+        <button onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
---
+++ first_app/src/Navigation.jsx
@@ -0,0 +1,11 @@
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Solid</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
